93af4cb1d54e50ceeb108b171fba2dde5fcc8705
briannaordaz <briannaordaz5@gmail.com>
2025-02-10 22:36:44 -0800

    Completed step 2

---
+++ COMP003A.LectureActivity5-2/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace COMP003A.LectureActivity5_2;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        internal class BankAccount
+        {
+            //Fields
+            private string _accountNumber;
+            private double _balance;
+            
+            //Properties
+            ///<summary>
+            /// Gets the account number.
+            /// </summary>
+
+            public string AccountNumber
+            {
+                get { return _accountNumber; }
+            }
+
+            ///<summary>
+            /// Gets or sets the account balance with validation.
+            /// </summary>
+
+            public double Balance
+            {
+                get { return _balance; }
+                set
+                {
+                    if (value >= 0)
+                        _balance = value;
+                }
+            }
+
+            /// <summary>
+            /// Initializes a new instance of BankAccount with an account number adn initial balance.
+            /// </summary>
+
+            public BankAccount(string accountNumber, double initialBalance)
+            {
+                //Set the account number and initial balance
+                _accountNumber = accountNumber;
+                //Validate the initial balance
+                Balance = initialBalance;
+            }
+        }
+    }
